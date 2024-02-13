02ef30b8f22b1eb51f03a1b94074a086b912a703
Pedro Leão <pedroforestileao@gmail.com>
2024-02-13 02:03:20 -0300

    created CMAKE project

---
+++ src/core/UUID.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hellhat on 2/13/24.
+//
+
+#include "UUID.h"
---
+++ src/core/UUID.h
@@ -0,0 +1,13 @@
+//
+// Created by hellhat on 2/13/24.
+//
+
+#ifndef PEDRO_DB_SRC_CORE_UUID_H_
+#define PEDRO_DB_SRC_CORE_UUID_H_
+
+class UUID
+{
+
+};
+
+#endif //PEDRO_DB_SRC_CORE_UUID_H_
