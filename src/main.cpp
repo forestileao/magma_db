f99673cc73f70379ecde73edf09f0c3857d3099d
Pedro Leão <pedroforestileao@gmail.com>
2024-02-13 02:05:06 -0300

    feature: added main test

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "core/Document.h"
+#include "core/DocumentValue.h"
+
+using PedroDb::Core::Document;
+using PedroDb::Core::DocumentValue;
+using PedroDb::Core::DocumentValueType;
+
+int main()
+{
+	Document document;
+	DocumentValue helloValue("Hello, World!");
+	DocumentValue another("Test");
+	DocumentValue number(42);
+	DocumentValue pi(3.14);
+	DocumentValue boolean(true);
+	DocumentValue array({helloValue, another, number, pi, boolean});
+	DocumentValue none;
+
+	Document anotherDocument("anotherId");
+	DocumentValue anotherDocumentValue(anotherDocument);
+
+	document.setProperty("anotherDocument", anotherDocumentValue);
+
+	document.setProperty("hello", helloValue);
+	document.setProperty("another", another);
+	document.setProperty("number", number);
+	document.setProperty("pi", pi);
+	document.setProperty("boolean", boolean);
+	document.setProperty("array", array);
+	document.setProperty("none", none);
+
+	// print all values
+	std::cout
+	<< "id: " << document.getId()
+	<< "\nHello: " << document["hello"].getString()
+	<< "\nAnother: " << document["another"].getString()
+	<< "\nNumber: " << document["number"].getInteger()
+	<< "\nPi: " << document["pi"].getDouble()
+	<< "\nBoolean: " << document["boolean"].getBool()
+	<< "\nInner Document Id: " << document["anotherDocument"].getDocument().getId()
+	<< std::endl;
+}
