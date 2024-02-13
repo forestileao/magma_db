d8ab0ec1f1291c8a952958a7b094ef7f4fe2b1f5
Pedro Leão <pedroforestileao@gmail.com>
2024-02-13 02:04:43 -0300

    feature: created UUID class

--- src/core/UUID.cpp
+++ src/core/UUID.cpp
@@ -2,4 +2,49 @@
 // Created by hellhat on 2/13/24.
 //
 
+// This implementation was got from https://stackoverflow.com/questions/24365331/how-can-i-generate-uuid-in-c-without-using-boost-library
+// TODO: create a better implementation that stores UUID bytes and check efficiency
+
 #include "UUID.h"
+#include <random>
+#include <sstream>
+
+using namespace PedroDb::Core;
+
+std::string UUID::generateUUID()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 15);
+	std::uniform_int_distribution<> dis2(8, 11);
+
+	std::stringstream ss;
+	int i;
+	ss << std::hex;
+	for (i = 0; i < 8; i++)
+	{
+		ss << dis(gen);
+	}
+	ss << "-";
+	for (i = 0; i < 4; i++)
+	{
+		ss << dis(gen);
+	}
+	ss << "-4";
+	for (i = 0; i < 3; i++)
+	{
+		ss << dis(gen);
+	}
+	ss << "-";
+	ss << dis2(gen);
+	for (i = 0; i < 3; i++)
+	{
+		ss << dis(gen);
+	}
+	ss << "-";
+	for (i = 0; i < 12; i++)
+	{
+		ss << dis(gen);
+	};
+	return ss.str();
+}
--- src/core/UUID.h
+++ src/core/UUID.h
@@ -1,13 +1,18 @@
 //
 // Created by hellhat on 2/13/24.
 //
+#include <string>
 
 #ifndef PEDRO_DB_SRC_CORE_UUID_H_
 #define PEDRO_DB_SRC_CORE_UUID_H_
 
-class UUID
+namespace PedroDb::Core
 {
-
-};
+	class UUID final
+	{
+	 public:
+		[[nodiscard]] static std::string generateUUID();
+	};
+}
 
 #endif //PEDRO_DB_SRC_CORE_UUID_H_
